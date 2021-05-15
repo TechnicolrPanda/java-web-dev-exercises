8a48eb98903892ebbd34157ab9bb7a9dfc190940
TechnicolrPanda <amelia.a.fitzpatrick@gmail.com>
2021-05-15 11:02:18 -0500

    Completed 2A exercises

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,9 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+    private String title;
+    private String description;
+    private ArrayList<Student> studentList;
+}
--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -2,6 +2,13 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class SchoolPractice {
     public static void main(String[] args) {
-        // Instantiate your Student class for part 2 here!
+
+        Student amelia = new Student();
+
+        amelia.setName("Amelia");
+        amelia.setStudentId(3411235);
+        amelia.setGpa(4.0);
+        amelia.setNumberOfCredits(1);
     }
 }
+
